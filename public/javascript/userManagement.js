330dc75188ba6d7aa276aa75553964756cc32ed8
raveendraHewage <hewagerv@gmail.com>
2021-02-12 00:12:40 +0530

    police ambulance requests search added

--- public/javascript/userManagement.js
+++ public/javascript/userManagement.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     setInterval(function() {
-        $("#col1").load('userrequests')
+        $("#col1").load('userrequests');
     }, 2000);
 
     setInterval(function() {
@@ -13,6 +13,7 @@ $(document).ready(function() {
             method: "post",
             data: { query: query },
             success: function(data) {
+                $("#col1").hide();
                 $('#result1').html(data);
             }
         });
@@ -24,6 +25,7 @@ $(document).ready(function() {
             load_data1(search1);
         } else {
             $("#result1").empty();
+            $("#col1").show();
         }
     });
 
@@ -33,6 +35,7 @@ $(document).ready(function() {
             method: "post",
             data: { query: query },
             success: function(data) {
+                $("#col2").hide();
                 $('#result2').html(data);
             }
         });
@@ -43,6 +46,7 @@ $(document).ready(function() {
         if (search1 != '') {
             load_data2(search1);
         } else {
+            $("#col2").show();
             $("#result2").empty();
         }
     });
